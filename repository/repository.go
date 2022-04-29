47aa497c377194d1ced2ad7bd2e4cba67d90cf9e
Pandu dwi Putra <pandu@pandudpn.id>
2022-04-29 15:29:07 +0700

    create interface all repository

---
+++ repository/repository.go
@@ -0,0 +1,17 @@
+package repository
+
+import (
+	"context"
+	
+	"github.com/pandudpn/blog/model"
+)
+
+type UserRepositoryInterface interface {
+	CreateUser(ctx context.Context, user *model.User) error
+	FindUserByEmail(ctx context.Context, email string) (*model.User, error)
+}
+
+type CacheRepositoryInterface interface {
+	GetDataFromAccessToken(ctx context.Context, accessToken string) (int, error)
+	SetLoginAccessToken(ctx context.Context, user *model.User) (model.ResponseLogin, error)
+}
